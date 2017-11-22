3582a23c4f716a3776d61b5a549a3923221193ae
Pr0methean <4961925+Pr0methean@users.noreply.github.com>
2017-11-22 14:54:27 -0800

    Add -v to cygcheck

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-c:\cygwin64\bin\sh -c "cygcheck -c; cygcheck -sr"
+c:\cygwin64\bin\sh -c "cygcheck -vc; cygcheck -vsr"
 echo ""
 echo "cygwin1.dll's:"
 for /r C:\ %%a in (*) do if "%%~nxa"=="cygwin1" echo "%%~dpnxa"
